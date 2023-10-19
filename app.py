8f9b2f1b9d31c8475233f2111855941c617c3572
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-10-19 20:40:50 +0600

    renamed

--- app.py
+++ app.py
@@ -3,81 +3,79 @@ from dotenv import load_dotenv
 if not load_dotenv():
     raise Exception("Failed to load .env file")
 #------------------------------------ .env file loaded------------------------------------
-from flask import Flask, render_template, request, redirect, make_response, Blueprint, g
-from flask_executor import Executor
-from api import api, Server, User, logging, os
 from settings import *
-
-#------------------------------------ App ------------------------------------
-class TukTukBot(Flask):
-    executor = None
+from api import api, Server, User
+from fastapi import FastAPI, responses, Request, Response
+from fastapi.templating import Jinja2Templates
+class TukTukBot(FastAPI):
+    """
+    This is the bot that will be used to extract the data from the wikipedia.
+    """
+    templates = Jinja2Templates("templates")
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.executor = Executor(self)
-        Server.init()
+        self.add_lifecycle()
 
-app = TukTukBot(__name__)
+    def add_lifecycle(self):
+        @self.on_event("startup")
+        def startup():
+            Server.init()
+            print("Starting up...")
 
+        @self.on_event("shutdown")
+        def shutdown():
+            print("Shutting down...")
 
-#------------------------------------Config------------------------------------
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['EXECUTOR_PROPAGATE_EXCEPTIONS'] = True
-app.config['EXECUTOR_MAX_WORKERS'] = 2
-app.config['EXECUTOR_TYPE'] = 'thread'
-app.config['EXECUTOR_MAX_TASKS'] = 2
-app.config['EXECUTOR_THREAD_NAME_PREFIX'] = 'TukTukBot'
-app.config['EXECUTOR_LOGGING_LEVEL'] = logging.INFO
-
-app.register_blueprint(api)
+app = TukTukBot()
+app.include_router(api)
 
 
-#------------------------------------ Web ------------------------------------
-@app.route('/')
-def index():
-    # Check if user is logged in
+
+@app.get("/", response_class=responses.HTMLResponse)
+async def home(request : Request):
+    stats = Server.get_stats()
     user = User.logged_in_user(request.cookies)
-    stats = None
-    if user:
-        stats = Server.get_stats()
-    return render_template('index.html', user=user, stats=stats)
+    return app.templates.TemplateResponse("index.html", context= {
+        'request' : request,
+        'user' : user,
+        'stats' : stats
+    })
 
-#------------------------------------ Tool Interface ------------------------------------
-@app.route("/interface")
-def interface():
-    if not User.logged_in_user(request.cookies):
-        return redirect(User.generate_login_url( '/tuktukbot'))
-    return render_template('interface.html',
-                           countries=AVAILABLE_COUNTRY,
-                           wikis=AVAILABLE_WIKI,
-                           topics=AVAILABLE_TOPICS
-                    )
-@app.route("/tuktukbot")
-def tuktukbot():
-    if not User.logged_in_user(request.cookies):
-        return redirect(User.generate_login_url( '/tuktukbot'))
-    return render_template('interface.html',
-                           countries=AVAILABLE_COUNTRY,
-                           wikis=AVAILABLE_WIKI,
-                           topics=AVAILABLE_TOPICS
-                    )
-#------------------------------------ OAuth2 Callback ------------------------------------
-@app.route("/user/callback")
-def login():
-    cookie_name, cookie_value, redirect_uri = User.generate_access_token(request.args)
-    response = make_response(redirect(redirect_uri))
-    response.set_cookie(cookie_name, cookie_value)
+def redirect_to(url, cookies : dict = {}):
+    response = responses.RedirectResponse(
+        url,
+    )
+    for key, value in cookies.items():
+        response.set_cookie(key, value)
     return response
+@app.get("/user/callback", response_class=responses.RedirectResponse)
+def login(code : str, state : str):
+    cookie_name, cookie_value, redirect_uri = User.generate_access_token(code = code, state = state)
+    return redirect_to(redirect_uri, {
+        cookie_name : cookie_value
+    })
 
 #------------------------------------ Logout ------------------------------------
-@app.post("/user/logout")
+@app.post("/user/logout", response_class=responses.RedirectResponse)
 def logout():
     cookie_name, cookie_value, redirect_uri = User.logout()
-    response = make_response(redirect(redirect_uri))
-    response.set_cookie(cookie_name, '', expires=0)
-    return response
-
-
-
-
-if __name__ == '__main__':
-    app.run(port=5000)
+    return redirect_to(redirect_uri, {
+        cookie_name : cookie_value
+    })
+@app.get("/tuktukbot", response_class=responses.HTMLResponse)
+async def tuktukbot(req : Request):
+    user = User.logged_in_user(req.cookies)
+    if user is None:
+        redirect_uri = User.generate_login_url('/tuktukbot')
+        return redirect_to(redirect_uri)
+    return app.templates.TemplateResponse("interface.html", context= {
+        'request' : req,
+        'user' : user,
+        'topics' : AVAILABLE_TOPICS,
+        'countries' : AVAILABLE_COUNTRY,
+        'wikis' : AVAILABLE_WIKI
+    })
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="", port=5000)
+    
