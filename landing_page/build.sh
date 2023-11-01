d03aab3200be54aa076a589b100fb3ecebc93e81
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-02 01:17:52 +0600

    added config properly

---
+++ landing_page/build.sh
@@ -0,0 +1,4 @@
+CURRENT_PATH=$(pwd)
+cd ..
+bash build.sh
+cd $CURRENT_PATH
--- landing_page/tailwind.config.js
+++ landing_page/tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fonts : {
+        primary : ['"Playfair Display']
+      },
+      colors : {
+        primary : 'rgb(0,102,153)',
+        secondary : 'rgb(153, 0, 0)',
+        fadedGreen : 'rgb(51 153 102)'
+      }
+    },
   },
   plugins: [],
 }
