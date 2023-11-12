3c6d5cc6c50e78e7d387f91d45cea89a7e076a59
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-13 02:46:20 +0600

    Added judgement

--- frontend/src/KitKat/Pages/Campaign/index.jsx
+++ frontend/src/KitKat/Pages/Campaign/index.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from "react-router";
+import { lazy } from "react";
 import ArticleSubmissionPage from "./ArticleSubmission";
+import JudgeSubmission from "./JudgeSubmission";
+// const JudgeSubmission = lazy(() => import("./JudgeSubmission"));
 const Campaign = () => {
     return (
         <Routes>
             <Route path='campaign/*'>
                 <Route path="submit" element={<ArticleSubmissionPage />} />
+                
                 <Route path="*" element={<div>Unknown Campaign Page</div>} />
             </Route>
         </Routes>
