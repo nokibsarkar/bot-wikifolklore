41a7cab054e8a26e2666362bb36b6a63d580c78c
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-13 21:48:00 +0600

    Added

--- app.py
+++ app.py
@@ -102,6 +102,13 @@ async def fnf(req : Request, optional_path : str = ''):
         redirect_uri = User.generate_login_url('/fnf')
         return redirect_to(redirect_uri)
     return responses.FileResponse("static/index.html")
+@app.get("/kitkat/{optional_path:path}", response_class=responses.HTMLResponse)
+async def kitkat(req : Request, optional_path : str = ''):
+    user = User.logged_in_user(req.cookies)
+    if user is None:
+        redirect_uri = User.generate_login_url('/kitkat')
+        return redirect_to(redirect_uri)
+    return responses.FileResponse("static/index.html")
 
 
 if __name__ == "__main__":
