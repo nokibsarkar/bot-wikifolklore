1d70e62fa17fcb86a095378caee9236937c36b69
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-21 01:55:38 +0600

    Kitkat: Add a Campaign Create Page Fixes #196

--- frontend/src/KitKat/Pages/Campaign/Configure/Create.jsx
+++ frontend/src/KitKat/Pages/Campaign/Configure/Create.jsx
@@ -1,11 +1,21 @@
+import { useCallback } from "react"
 import EditableCampaign from "./EditableCampaign"
+import KitKatServer from "../../../Server";
 
 const CampaignCreate = () => {
+    const handleSave = useCallback((campaign, setLoading) => {
+        (async () => {
+            setLoading(true);
+            const newCampaign = await KitKatServer.Campaign.createCampaign(campaign);
+            setLoading(false);
+            const url = new URL("/kitkat/campaign/" + newCampaign.id, window.location.origin);
+            document.location.href = url;
+        })();
+    }, [])
     return (
         <div>
             <h1>Create Campaign</h1>
-            <EditableCampaign minimumStep={0} linear={true} />
-
+            <EditableCampaign minimumStep={0} linear={true} defaultStep={0} showActions={false} onSave={handleSave} />
         </div>
     )
 }
