771bf5a6f46cbbf2d034a117c3501fefd1bc70de
Nazmul Haque Naqib <nokibsarkar@gmail.com>
2023-11-20 23:27:03 +0600

    Add all the restrictions as checkbox. `minimumSubmissionPerArticle` should have a number box as input along with a label Fixes #199

--- frontend/src/KitKat/Pages/Campaign/Configure/Restrictions.jsx
+++ frontend/src/KitKat/Pages/Campaign/Configure/Restrictions.jsx
@@ -1,25 +1,140 @@
 import { TextField } from "@mui/material";
 import UserInput from "../../../Components/UserInput";
 import BlackListIcon from '@mui/icons-material/Block';
-const CampaignRestrictions = ({campaign, campaignDispatch}) => {
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+const CampaignRestrictions = ({ campaign, campaignDispatch }) => {
     return (
-        <>
-            <TextField
-                label="Maximum Submission of Same Article"
-                variant="outlined"
-                fullWidth
-                value={campaign.maximumSubmissionOfSameArticle}
-                onChange={(e) => {
-                    campaignDispatch({ type: 'maximumSubmissionOfSameArticle', payload: e.target.value })
-                }}
+        <FormGroup>
+            <FormControlLabel
+                sx={{ m: 1 }}
+                control={
+                    <Checkbox
+                        checked={campaign.allowExpansions}
+                        onChange={(e) => {
+                            campaignDispatch({ type: 'allowExpansions', payload: e.target.checked })
+                        }}
+                    />
+                }
+                label="Allow users to submit articles that were not created rather expanded"
             />
+            {campaign.allowExpansions ? <FormGroup sx={{
+                my: 1, display: 'flex', flexDirection: {
+                    xs: 'column',
+                    sm: 'row'
+
+                }, justifyContent: 'space-evenly'
+            }}>
+
+                <TextField
+                    label="Minimum Added Bytes"
+                    variant="outlined"
+                    sx={{
+                        my: 1, width: {
+                            xs: '100%',
+                            sm: '40%'
+                        }
+                    }}
+                    fullWidth
+                    inputMode="numeric"
+                    size="small"
+                    helperText="The number of bytes the participant must add on the submitted article since the beginning of the campaign"
+                    value={campaign.minimumAddedBytes}
+                    onChange={(e) => {
+                        campaignDispatch({ type: 'minimumAddedBytes', payload: e.target.value })
+                    }}
+                />
+                <TextField
+                    label="Minimum Added Words"
+                    variant="outlined"
+                    sx={{
+                        my: 1, width: {
+                            xs: '100%',
+                            sm: '40%'
+                        }
+                    }}
+                    fullWidth
+                    inputMode="numeric"
+                    size="small"
+
+                    helperText="The number of words the participant must add on the submitted article since the beginning of the campaign"
+                    value={campaign.minimumAddedWords}
+                    onChange={(e) => {
+                        campaignDispatch({ type: 'minimumAddedWords', payload: e.target.value })
+                    }}
+                />
+            </FormGroup> :
+                <FormGroup sx={{
+                    my: 1, display: 'flex', flexDirection: {
+                        xs: 'column',
+                        sm: 'row'
+
+                    }, justifyContent: 'space-evenly'
+                }}>
+                    <TextField
+                        label="Minimum Total Bytes"
+                        variant="outlined"
+                        sx={{
+                            my: 1, width: {
+                                xs: '100%',
+                                sm: '40%'
+                            }
+                        }}
+                        fullWidth
+                        inputMode="numeric"
+                        size="small"
+                        helperText="The number of bytes of the submitted article"
+                        value={campaign.minimumTotalBytes}
+                        onChange={(e) => {
+                            campaignDispatch({ type: 'minimumTotalBytes', payload: e.target.value })
+                        }}
+                    />
+                    <TextField
+                        label="Minimum Total Words"
+                        variant="outlined"
+                        sx={{
+                            my: 1, width: {
+                                xs: '100%',
+                                sm: '40%'
+                            }
+                        }}
+                        fullWidth
+                        inputMode="numeric"
+                        size="small"
+                        helperText="The number of words of the submitted article"
+                        value={campaign.minimumTotalWords}
+                        onChange={(e) => {
+                            campaignDispatch({ type: 'minimumTotalWords', payload: e.target.value })
+                        }}
+                    />
+
+                </FormGroup>
+            }
+
+
+            {/* <FormControlLabel
+            sx = {{m : 1}}
+                control={
+                    <Checkbox
+                        checked={campaign.allowSubmission}
+                        onChange={(e) => {
+                            campaignDispatch({ type: 'allowSubmission', payload: e.target.checked })
+                        }}
+                    />
+                }
+                label="Can the same article be submitted multiple times?"
+            /> */}
+
             <UserInput
+            backgroundColor="pink"
+            // color="white"
                 users={campaign.blackListedUsers}
                 onChange={blackListedUsers => campaignDispatch({ type: 'blackListedUsers', payload: blackListedUsers })}
                 icon={<BlackListIcon />}
-                fieldName='Black Listed Users'
+                fieldName='Block the follwoing users from participating in the campaign (optional)'
             />
-        </>
+        </FormGroup>
     )
 }
 export default CampaignRestrictions;
