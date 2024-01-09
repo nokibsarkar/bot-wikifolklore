3e739fb03bc690149c7c734b74d90fe2d6382421
Nazmul Haque Naqib <39087050+nokibsarkar@users.noreply.github.com>
2024-01-10 04:11:08 +0600

    Update YearStatistics.jsx

--- frontend/src/CampWiz/Pages/Statistics/YearStatistics.jsx
+++ frontend/src/CampWiz/Pages/Statistics/YearStatistics.jsx
@@ -1,7 +1,7 @@
 import Statistics from "./Statistics";
 import { Link } from "react-router-dom";
 const column2 = [
-    {field : 'color', headerName : 'Color', align : 'center', headerAlign : 'center', minWidth : 70, sortable : false, flex : 0.2, renderCell : (params) => <span style={{backgroundColor : params.value, width : '100%', height : '100%'}}></span>},
+    {field : 'color', headerName : 'Color', align : 'center', headerAlign : 'center', minWidth : 70, sortable : false, renderCell : (params) => <span style={{backgroundColor : params.value, width : '100%', height : '100%'}}></span>},
     { align : 'center', headerAlign : 'center',field: 'name', headerName: 'Name', sortable : false, minWidth: 100, flex: 1, renderCell : (params) => {
         const campaignID = params.row.id;
         const campaignName = params.value;
